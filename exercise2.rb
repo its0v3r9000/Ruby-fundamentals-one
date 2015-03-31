d6022aa3e0b9863b4a77e6ef85d80aa98b0602e6
Cody <codyjjwbrown@gmail.com>
2015-03-31 14:11:15 -0400

    exercises 1 and 2

---
+++ exercise2.rb
@@ -0,0 +1,18 @@
+# Question 1 - the tip!
+meal = 55
+tip = 1.15
+total = meal * tip
+puts total
+
+#Question 2 - Addition of s n i
+stuff = "this is a thing"
+numbers = 5
+puts "#{stuff} + #{numbers}"
+
+#Question 3 - Big numbers with inerpolation
+number1 = 45628
+number2 = 7839
+puts "#{number1} * #{number2} = #{number1 * number2}"
+
+#4 My guess is false
+puts (true && false) || (false && true) || !(false && false)
